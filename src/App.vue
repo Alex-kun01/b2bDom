c1b4cf9b8baf111afa306c510720a0e8d4f6176a
蒋治坤 <1790557086@qq.com>
2020-03-05 12:18:22 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="box">我是一段测试less语法的文字</div>
+    <el-button type="primary">按钮</el-button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+#app {
+  .box{
+    color: green;
+    font-size: 30px;
+    font-weight: blod;
+  }
+}
+</style>
